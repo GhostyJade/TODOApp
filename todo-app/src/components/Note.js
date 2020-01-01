dc92a9a0b7160057b8426d21e6688c74c94e262c
GhostyJade <giandreas@outlook.it>
2020-01-01 21:20:09 +0100

    Added delete and completed features

--- todo-app/src/components/Note.js
+++ todo-app/src/components/Note.js
@@ -4,13 +4,30 @@ import '../style/note.css'
 
 class Note extends Component {
 
-    //completed updating
+    
 
+    deleteNote() {
+        this.props.delete(this.props.title)
+    }
+
+    checked(e) {
+        this.props.onComplete(this.props, e.target.checked)
+    }
+
+    //TODO consider \n when saving a note
+    //add trash can icon
     render() {
         return (
             <div className="note">
                 <div className="note-header">
+                    <input className="completed-box" type="checkbox" defaultChecked={this.props.completed} onChange={this.checked.bind(this)} />
                     <h3 className="note-title">{this.props.title}</h3>
+                    <button className="btn-container-square btn-container-28 link-button" onClick={() => this.deleteNote()}>
+
+                    </button>
+                    <div className="delete">
+
+                    </div>
                 </div>
                 <div className="note-content">
                     <p>{this.props.content}</p>
--- todo-app/src/components/NoteVisualizer.js
+++ todo-app/src/components/NoteVisualizer.js
@@ -1,14 +1,14 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 
 import Note from './Note'
 
 class NoteVisualizer extends Component {
 
     render() {
-        const items = this.props.notes.map(e=>{
-            return <Note key={e.title} title={e.title} content={e.content} completed={e.completed} />
+        const items = this.props.notes.map(e => {
+            return <Note key={e.title} title={e.title} content={e.content} completed={e.completed} delete={(e) => this.props.delete(e)} onComplete={(title, value) => this.props.update(title, value)} />
         })
-        return(
+        return (
             items
         )
     }
